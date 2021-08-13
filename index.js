2aad4cb7230f1c809a43ea7afd69f5ab521ecbb7
eghak <88344325+eghak@users.noreply.github.com>
2021-08-13 09:21:38 -0700

    Git info
    Git info

--- index.js
+++ index.js
@@ -1,35 +1,95 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
 //////////////////PROBLEM 1////////////////////
-/* Create a variable called 'myName' with a value that is your name as a string. */
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
 
-let myName = `Egha Kusuma`
+let gitDefinition = `version control system`
+
 //////////////////PROBLEM 2////////////////////
-/* Create a variable called 'faveNum' with a value that is your favorite number. */
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
 
-let faveNum = 10
+let gitHubDefinition = `git repository hosting service`
+
+//////////////////PROBLEMS 3 - 8////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
 
 //////////////////PROBLEM 3////////////////////
-/* Create a variable called 'lovesCode' and assign it a boolean value. */
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
 
-let lovesCode = true
+const init = {
+  description: `initialize a local repository right here in this folder`,
+  code: `git init`
+};
 
 //////////////////PROBLEM 4////////////////////
 /*
-  Write an if-statement that checks if the faveNum variable is 
-  equal to 13. If it is, console.log('lucky 13!'). 
-  If it's not, console.log('not lucky 13').
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
 */
 
-if (faveNum === 13) {
-  console.log(`lucky 13!`)
-} else {
-  console.log(`not lucky 13`)
-}
+const clone = {
+  description: `copy a repo from Githun and put it on my machine`,
+  code: `git clone REPO_URL`
+};
 
 //////////////////PROBLEM 5////////////////////
 /*
-  Write a for loop that prints out your faveNum 5 times.
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
 */
 
-for (let i=0; i < 5; i++) {
-  console.log(faveNum)
-}
+const status = {
+  description: `tell me about this repo`,
+  code: `git status`
+};
+
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+const add = {
+  description: `add some files to be tracked`,
+  code: `git add`
+};
+
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+const commit = {
+  description: `create a snapshot of the files I'm tracking`,
+  code: `git commit`
+};
+
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+const push = {
+  description: `put my local commits on Github`,
+  code: `git push REPO_NAME`
+};
